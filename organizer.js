fe10129f853257ae6b01028e392507bcaad9d27b
Piotr Brzeziński <peb.brzezinski@gmail.com>
2017-01-24 14:47:32 +0100

    Update organizer.js

--- organizer.js
+++ organizer.js
@@ -1,28 +1,30 @@
-const main = document.getElementsByClassName('main')[0];
+(function () {
+    const main = document.getElementsByClassName('main')[0];
 
-function goToMain(){
-    const lockScr = document.getElementsByClassName('lockScreen')[0];
-    lockScr.style.display = 'none';
-    main.style.visibility = 'visible';
-}
+    function goToMain(){
+        const lockScr = document.getElementsByClassName('lockScreen')[0];
+        lockScr.style.display = 'none';
+        main.style.visibility = 'visible';
+    }
 
 
-function doSth (event){
-    const x = event.keyCode;
-    if(x === 13){
-        const loginBut = document.getElementsByClassName('loginBut')[0];
-        loginBut.click();
+    function doSth (event){
+        const x = event.keyCode;
+        if(x === 13){
+            const loginBut = document.getElementsByClassName('loginBut')[0];
+            loginBut.click();
+        };
     };
-};
 
-function logIn (){
-    const user = document.getElementById('userName');
-    const pass = document.getElementById('password');
-    console.log(user.value);
-    console.log(pass.value);
-    if(user.value === 'admin' && pass.value === 'admin'){
-        console.log('test');
-    } else {
-        alert('Wrong user name or passwod. Try again.');
+    function logIn (){
+        const user = document.getElementById('userName');
+        const pass = document.getElementById('password');
+        console.log(user.value);
+        console.log(pass.value);
+        if(user.value === 'admin' && pass.value === 'admin'){
+            console.log('test');
+        } else {
+            alert('Wrong user name or passwod. Try again.');
+        };
     };
-};
+})
